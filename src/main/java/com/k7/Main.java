29b32b58ecbea9d700251629f9528c7d0d4a793d
NickSemerenko <nickolay.sem@gmail.com>
2020-12-16 23:03:03 +0200

    add parseList

--- src/main/java/com/k7/Main.java
+++ src/main/java/com/k7/Main.java
@@ -2,7 +2,9 @@ package com.k7;
 
 public class Main {
     public static void main(String[] args) {
-UserParser userParser = new UserParser();
-        System.out.println(userParser.parse("programmer@asdasd"));
+        UserParser userParser = new UserParser();
+        String inputString = "programmer:vasia@123,vaa:@t64654,programmer:vasia," +
+                "vasia@asdasd,@t61214654,vasia,vasia:@trulala,programmer:vova@654235asd";
+        userParser.viewUsers(userParser.parselist(inputString));
     }
 }
--- src/main/java/com/k7/UserParser.java
+++ src/main/java/com/k7/UserParser.java
@@ -2,6 +2,8 @@ package com.k7;
 
 import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,21 +15,37 @@ public class UserParser {
         Pattern pattern = Pattern.compile("(\\w+)?(?::(\\w+))?(?:@(\\w+))?");
         Matcher matcher = pattern.matcher(user);
         User parsedUser = null;
-        if (!matcher.matches()) {
-            System.out.println("Invalid user!");
+        if ((!matcher.matches()) || (matcher.group(1) == null && matcher.group(2) == null)) {
+            System.out.println(user + " - Invalid user!");
             return null;
-        }
-        else {
-            if (matcher.group(2)==null) {
-                parsedUser = new User(matcher.group(1), matcher.group(1),matcher.group(3));
+        } else {
+            if (matcher.group(2) == null) {
+                parsedUser = new User(matcher.group(1), matcher.group(1), matcher.group(3));
             }
             if (matcher.group(1) == null) {
                 parsedUser = new User(matcher.group(2), matcher.group(2), matcher.group(3));
             }
-            if (matcher.group(1) !=null&&matcher.group(2)!=null) {
+            if (matcher.group(1) != null && matcher.group(2) != null) {
                 parsedUser = new User(matcher.group(1), matcher.group(2), matcher.group(3));
             }
             return parsedUser;
+        }
+    }
+
+    public List<User> parselist(String string) {
+        List<User> userList = new ArrayList<>(0);
+        for (String s : string.split(",")
+        ) {
+            if (parse(s) != null)
+                userList.add(parse(s));
+        }
+        return userList;
+    }
+
+    public void viewUsers(List<User> users) {
+        for (User u : users
+        ) {
+            System.out.println(u);
         }
     }
 }
